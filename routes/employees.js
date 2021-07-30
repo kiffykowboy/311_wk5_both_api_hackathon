f40d8d576b926788aa667de5c962f882b9897c58
SLISETTE127 <SLISETTE127@GMAIL.COM>
2021-07-29 20:02:20 -0500

    routes for employee by first name

--- routes/employees.js
+++ routes/employees.js
@@ -3,4 +3,10 @@ const router = express.Router()
 
 const employeesController = require('../controllers/employees')
 
+
+router.get('/controllers/employees', (req, res) => {
+  
+  res.json(user)
+})
+
 module.exports = router;
