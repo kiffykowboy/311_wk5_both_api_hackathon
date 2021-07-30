43b87430cd83525c368a471ba16d511c0c7463ca
Chase <landon.powell14@gmail.com>
2021-07-29 19:50:16 -0500

    fixed controller and route for employee by id

--- Routers/employees.js
+++ Routers/employees.js
@@ -1,2 +1,3 @@
+
 const router = require('./employees/id')
 router.get('./employees:id.getEmployeesById')
--- controllers/employees.js
+++ controllers/employees.js
@@ -1,1 +1,4 @@
 module.exports = { getEmployees, getEmployeesById, getEmployeesByFirstName }
+
+
+const getEmployeesById = res.json(employees.find(employees => employees.id == req.params.id))
