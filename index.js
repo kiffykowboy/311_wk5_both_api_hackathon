726aa5e56504c178fb685da621f56a21cc0de4c4
mayrant <mayrant2004@yahoo.com>
2021-07-29 20:31:18 -0500

    added import statement for router to index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const employees = require("./routes/employees");
+
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(express.json());
+app.use(employees);
 
 app.listen(port, () => console.log("Server started"));
