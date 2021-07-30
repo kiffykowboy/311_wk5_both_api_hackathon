397a7e60d4bd38a6e93e67d4b6d69350aa1366e5
Sam Dunegan <sam.dunegan@sbcglobal.net>
2021-07-29 19:18:50 -0500

    GET routs for employees

--- controllers/employees.js
+++ controllers/employees.js
@@ -1,1 +1,5 @@
+
+
+
+
 module.exports = { getEmployees, getEmployeesById, getEmployeesByFirstName }
--- routes/employees.js
+++ routes/employees.js
@@ -3,4 +3,10 @@ const router = express.Router()
 
 const employeesController = require('../controllers/employees')
 
+router.get("/employee/:id", employeesController.getEmployee);
+
+router.get("/employee/name/:firstName", employeesController.getEmployeesByName);
+
+
+
 module.exports = router;
