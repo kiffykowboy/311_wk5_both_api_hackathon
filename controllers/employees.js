7e77346b470fa4a09a1e2ce8fa04acbd4646b597
Sam Dunegan <sam.dunegan@sbcglobal.net>
2021-07-29 19:20:52 -0500

    Controller function getEmployees

--- controllers/employees.js
+++ controllers/employees.js
@@ -1,5 +1,23 @@
 
+//Controller Function getEmployees
 
+let getEmployees = function(req, res){;
+  console.log("Inside GET employee by id")
+  let id = req.params.id
+  let sql = "SELECT * FROM employees WHERE emp_no = ?"
+  connection.query(sql, [id], (error, rows) => {
+    console.log("ROWS:", rows)
+    if(error){
+      console.error("failed", error);
+      res.sendStatus(500);
+    }else if (!rows || rows.length == 0){
+      res.sendStatus(404);
+    }else {
+      res.send(rows[0]);
+    }
+  })
+  
+  };
 
 
 module.exports = { getEmployees, getEmployeesById, getEmployeesByFirstName }
